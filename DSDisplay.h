204479b531d899bd2bb2bcd3b1b41ee1445a5714
Lukaß Zhang <Sunny_Clouds@outlook.com>
2023-12-19 20:39:51 +0800

    添加项目文件。

---
+++ DSDisplay.h
@@ -0,0 +1,22 @@
+/**********************************************************************
+* The fundamental header file of all other header files of Data
+* Structure Display.
+*
+* Yuhao Zhang. 12/19/2023
+**********************************************************************/
+#pragma once
+#ifndef _DSDISPLAY_H_
+#define _DSDISPLAY_H_
+
+#pragma warning(disable:6385)
+#pragma warning(disable:6386)
+#pragma warning(disable:4244)
+#pragma warning(disable:4267)
+#pragma warning(disable:4305)
+
+#define SHOW_CONSOLE
+#define _CRT_SECURE_NO_WARNINGS
+#define _NAMESPACE_DSD_BEGIN    namespace dsd {
+#define _NAMESPACE_DSD_END      }
+
+#endif // !_DSDISPLAY_H_
